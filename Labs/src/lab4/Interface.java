c54a2e1ed5edf1c96b46cb5fa0a45b20e6c4d4a9
Miguel Botelho <m_botelho8@hotmail.com>
2016-03-17 11:39:23 +0000

    Interface done

--- Labs/src/lab4/Interface.java
+++ Labs/src/lab4/Interface.java
@@ -1,9 +1,14 @@
 package lab4;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * Created by m_bot on 17/03/2016.
  */
-public class Interface implements Remote {
+public interface Interface extends Remote {
+
+    int register(String plate, String owner) throws RemoteException;
+
+    String lookup(String plate) throws RemoteException;
 }
