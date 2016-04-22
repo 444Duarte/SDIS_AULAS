8cd9df3abcaa046fd714dbbe2a9a8bb866590159
Miguel Botelho <m_botelho8@hotmail.com>
2016-04-22 13:09:10 +0100

    think client is done. missing server.

--- Labs/src/lab5/SSLClient.java
+++ Labs/src/lab5/SSLClient.java
@@ -2,12 +2,12 @@ package lab5;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.Provider;
+import java.security.Security;
 
 /**
  * Created by Miguel on 10/03/2016.
@@ -23,13 +23,23 @@ public class SSLClient {
         args[4] = "pass";
         args[5] = "SHA-512";
 
-        SSLSocket s = null;
+        PrintWriter out = null;
+        SSLSocket sock = null;
         SSLSocketFactory ssf = null;
 
         ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
 
         try {
-            s = (SSLSocket) ssf.createSocket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+            sock = (SSLSocket) ssf.createSocket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+            sock.startHandshake();
+
+            out = new PrintWriter(sock.getOutputStream(), true);
+
+            for (int i = 2; i < args.length - 1; i++) {
+                out.println(args[i]);
+            }
+            out.close();
+            sock.close();
         }
         catch( IOException e) {
             System.out.println("Server - Failed to create SSLSocket");
